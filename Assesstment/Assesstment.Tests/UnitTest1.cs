aa0ff1c2543eab92b6a35abfc4dd1123d069c460
Enrique Cruz <enrique.cs095@unitec.edu>
2021-12-06 23:34:14 +0000

    Added testing function

--- Assesstment/Assesstment.Tests/UnitTest1.cs
+++ Assesstment/Assesstment.Tests/UnitTest1.cs
@@ -6,13 +6,67 @@ namespace Assesstment.Tests
     public class UnitTest1
     {
 
-         [Fact]
-        public void Test_If_Add_Data( )
+        // Given the user input is empty when calculating the sum then it should return zero.
+        [Fact]
+        public void Input_is_Empty()
         {
-         
+            // Arrange
+            var calc = new Calculator();
+            var expect = 0;
 
+            // Act
+            var result = calc.Sum();
+            // Assert
+
+            Assert.Equal(expect, result);
         }
 
 
+        // Given the user input is one number when calculating the sum then it should return the same number. (example "3" should equal 3)
+
+        [Fact]
+        public void Input_is_One_Number_Should_Return_Same_Number()
+        {
+            // Arrange
+            var calc = new Calculator();
+            var expect = 3;
+
+            // Act
+            var result = calc.Sum("3");
+            // Assert
+
+            Assert.Equal(expect, result);
+        }
+
+        // Given the user input is two numbers when calculating the sum then it should return the sum of those numbers. (example "1,2" should equal 3)
+
+        [Fact]
+        public void Input_is_Two_Numbers_Should_Return_Sum_Of_Them()
+        {
+            // Arrange
+            var calc = new Calculator();
+            var expect = 3;
+
+            // Act
+            var result = calc.Sum("1,2");
+            // Assert
+
+            Assert.Equal(expect, result);
+        }
+
+        // Given the user input is an unknown amount of numbers when calculating the sum then it should return the sum of all the numbers. (example "1,2,3" should equal 6)
+        [Fact]
+        public void Input_is_Unknown_Amount_Of_Numbers_Should_Return_Sum_Of_Them()
+        {
+            // Arrange
+            var calc = new Calculator();
+            var expect = 6;
+
+            // Act
+            var result = calc.Sum("1,2,3");
+            // Assert
+
+            Assert.Equal(expect, result);
+        }
     }
 }
--- Assesstment/Assesstment/Assesstment.cs
+++ Assesstment/Assesstment/Assesstment.cs
@@ -2,9 +2,18 @@
 
 namespace Assesstment
 {
-    public class Assesstment: IAssesstment
+    public class Calculator
     {
 
-        
+        public int Sum(string input="0"){
+            char delimiter=',';
+            var stringSplit = input.Split(delimiter);
+            var result=0;
+            foreach(var @char in stringSplit){
+                result+= Int32.Parse(@char.ToString());
+            }
+        return result;
+        }
+      
     }
 }
